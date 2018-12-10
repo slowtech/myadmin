460b5d1fd047a1b6c60b954a0a8fb21af64dc6c8
slowtech <ivictor@foxmail.com>
2018-12-11 06:21:14 +0800

    related to replication

---
+++ cmd/replication.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/slowtech/myadmin/common"
+	"github.com/spf13/cobra"
+	"time"
+)
+
+var (
+	ReplicationCmd = &cobra.Command{
+		Use:   "replication",
+		Short: "Deploy replication",
+		Long:  `Deploy replication`,
+		Run:   DeployReplication,
+	}
+	deploy_repl_cnf    string
+	deploy_repl_binary string
+)
+
+func init() {
+	deployCmd.AddCommand(ReplicationCmd)
+	ReplicationCmd.Flags().StringVarP(&deploy_repl_cnf, "defaults-file", "c", "", "The default config file")
+	ReplicationCmd.Flags().StringVarP(&deploy_repl_binary, "binary", "b", "", "The MySQL Binary tarball. If not specified, Assume the binary files are already in basedir")
+	ReplicationCmd.MarkFlagRequired("defaults-file")
+
+}
+
+func DeployReplication(cmd *cobra.Command, args []string) {
+	var host common.Host
+	host.Connection = common.GetClient("192.168.244.30","22","root","123456")
+	host.Run("df -h")
+	t1 := time.Now().Unix()
+	host.Scp("/usr/local/goland-2018.3.1.tar.gz","/tmp/goland-2018.3.1.tar.gz")
+	//mysql.DeployInstance(deploy_single_binary,deploy_single_cnf)
+	t2 := time.Now().Unix()
+	fmt.Println(t2-t1)
+}
+
